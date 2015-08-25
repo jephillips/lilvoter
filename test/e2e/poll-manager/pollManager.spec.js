bf88ee31329e7f6edfcfd59d5b4508cef96aab08
Josh <jephillips@cohesionforce.com>
2015-08-25 10:25:18 -0500

    Fixed repeater test

--- test/e2e/poll-manager/pollManager.spec.js
+++ test/e2e/poll-manager/pollManager.spec.js
@@ -10,21 +10,15 @@ describe("Poll Manager", function () {
         beforeEach(function () {
             button = element(by.id('save-button'));
             input = element(by.id('title-input'));
-            //pollList = element.all(by.repeater('poll in managerCtrl.polls').column('poll.title').map(function(element){
-            //    return element.getText()}))
-        });
+            pollList = element.all(by.repeater('poll in managerCtrl.polls'))});
 
-        // Need to learn how to test repeater
-        //it("should add a new element to polls when clicked", function () {
-        //
-        //    input.sendKeys('New item');
-        //    button.click();
-        //
-        //    console.log("Poll List")
-        //    console.log(pollList);
-        //
-        //    expect(pollList.toContain('New item'));
-        //});
+        it("should add a new element to polls when clicked", function () {
+
+            input.sendKeys('New item');
+            button.click();
+
+            expect(pollList.get(2).getText()).toContain('New item');
+        });
 
         it("should clear the input once save is clicked", function () {
 
