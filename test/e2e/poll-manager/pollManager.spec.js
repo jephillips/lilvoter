8517f7605539f01fbb6397e7ff682cc9c896ee61
Josh <jephillips@cohesionforce.com>
2015-08-25 10:42:57 -0500

    Moved to Page Object pattern

--- test/e2e/poll-manager/pollManager.spec.js
+++ test/e2e/poll-manager/pollManager.spec.js
@@ -1,30 +1,49 @@
 /**
  * Created by josh on 8/24/15.
  */
+
+function PollManagerPage(){
+    this.button = element(by.id('save-button'));
+    this.input = element(by.id('title-input'));
+    this.pollList = element.all(by.repeater('poll in managerCtrl.polls'));
+
+    this.get = function(){
+        browser.get('/')
+    };
+
+    this.clickButton = function(){
+     this.button.click();
+    }
+
+    this.sendInput = function(input){
+        this.input.sendKeys(input)
+    }
+}
+
+
 describe("Poll Manager", function () {
+
+    var page = new PollManagerPage();
+
     describe('save button', function () {
-        var button,
-            input,
-            pollList;
 
         beforeEach(function () {
-            button = element(by.id('save-button'));
-            input = element(by.id('title-input'));
-            pollList = element.all(by.repeater('poll in managerCtrl.polls'))});
+            page.get();
+        });
 
         it("should add a new element to polls when clicked", function () {
 
-            input.sendKeys('New item');
-            button.click();
+            page.sendInput('New item');
+            page.clickButton();
 
-            expect(pollList.get(2).getText()).toContain('New item');
+            expect(page.pollList.get(2).getText()).toContain('New item');
         });
 
         it("should clear the input once save is clicked", function () {
 
-            input.sendKeys('Hello');
-            button.click();
-            expect(input.getText()).toBe('');
+            page.sendInput('Cleared Text');
+            page.clickButton();
+            expect(page.input.getText()).toBe('');
         })
 
 
