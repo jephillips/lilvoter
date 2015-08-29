299f9d5c5a8f13f16c77b6d0d501e6b694ac330e
Jeremy Atkinson <jchatkinson@gmail.com>
2015-08-29 12:13:20 -0700

    merge and shuffled files from master

--- client/components/common/services/pollService.js
+++ client/components/common/services/pollService.js
@@ -4,17 +4,21 @@
 
 export default angular => {
     angular.module('app.service.pollservice', [
-        'app.model.poll'
-    ]).service('pollService', function (Poll, $http, $q) {
+    'app.model.poll'
+]).service('pollService', function (Poll, $http, $q) {
 
         var service = this,
             polls = [new Poll("Poll 1"), new Poll("Poll 2")],
             URLS = {
-                FETCH: 'data.json'
+                //            FETCH: 'data.json'
+                FETCH: 'http://localhost:3000/polls',
+                POST: 'http://localhost:3000/polls'
             };
 
         service.getPollList = function () {
-            return (polls) ? $q.when(polls) : $http.get(URLS.FETCH).then(cacheResults);
+            //        return (polls) ? $q.when(polls) : $http.get(URLS.FETCH).then(cacheResults); //TODO: add a error callback if request failed
+            return $http.get(URLS.FETCH).then(cacheResults)
+
         };
 
         function extractData(result) {
@@ -22,7 +26,11 @@ export default angular => {
         }
 
         function cacheResults(result) {
+            console.log('result is:');
+            console.log(result);
             polls = extractData(result);
+            console.log('polls is:');
+            console.log(polls);
             return polls;
         }
 
@@ -30,11 +38,19 @@ export default angular => {
 
         service.addPollPlaceholder = function (title) {
             // Need the backend to handle post request -- All you Jer
-            polls.push(new Poll(title));
+            //        polls.push(new Poll(title));
+            var poll = new Poll(title);
+            return $http.post(URLS.FETCH, poll).then(function (data) {
+                console.log('successful POST of:');
+                console.log(poll);
+                polls.push(poll);
+            });
+
         };
 
         service.getPollsPlaceholder = function () {
-            return polls;
+            //        return polls;
+            return $http.get(URLS.FETCH).then(cacheResults);
         };
     });
 }
