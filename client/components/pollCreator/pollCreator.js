c6df4e217e102eddb329e167bc56f4360b507245
Josh <jephillips@cohesionforce.com>
2015-08-29 17:32:25 -0500

    Saving poll takes you back to poll list

--- client/components/pollCreator/pollCreator.js
+++ client/components/pollCreator/pollCreator.js
@@ -2,7 +2,7 @@
  * Created by josh on 8/29/15.
  */
 
-var CreateController = function(pollService){
+var CreateController = function(pollService, $state){
     var ctrl = this;
 
     ctrl.create = function(){
@@ -19,10 +19,15 @@ var CreateController = function(pollService){
     ctrl.addOption = function(){
         ctrl.options.push({position: ctrl.options.length});
         console.log(ctrl.options);
-    }
+    };
 
     ctrl.submit = function(){
+        if(ctrl.title != ''){
         pollService.addPoll(ctrl.title, ctrl.options)
+        $state.go('home');
+        } else {
+            alert("Title must not be empty")
+        }
     }
 };
 
