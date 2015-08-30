01931a7877dbf074b80aea83fed9f104d9b785cb
Josh <jephillips@cohesionforce.com>
2015-08-30 11:20:33 -0500

    Created basic voting template

--- client/components/vote/vote.js
+++ client/components/vote/vote.js
@@ -2,11 +2,22 @@
  * Created by josh on 8/29/15.
  */
 
-var VoteController = function (pollService, $stateParams) {
+var VoteController = function (pollService, $stateParams, $state) {
     var ctrl = this;
 
-    console.log($stateParams);
-    ctrl.currentItem = pollService.getPollById($stateParams.id)
+    pollService.getPollById($stateParams.id)
+        .then(function (result) {
+            ctrl.currentPoll = result;
+        });
+
+    ctrl.submit = function(){
+        console.log('Submit');
+        if (ctrl.choice){
+            $state.go('home');
+        } else {
+            alert('You must select an option!');
+        }
+    }
 
 };
 
